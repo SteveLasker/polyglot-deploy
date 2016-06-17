7aee3afb71c752b2eebb7c387f6af4cfe2cf34a9
David Zhao <dazhao@microsoft.com>
2016-06-17 14:30:09 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # The authentication part is just a PoC.
-SSH_AGENT_PID=$(eval `ssh-agent` | awk '{print $3}')
+eval `ssh-agent`
 ssh-add $ACS_SSHKEY
 
 scp -P 2200 $(pwd)/docker-compose.yml $ACS_MASTER0:~/polyglot/docker-compose.yml
 scp -P 2200 $(pwd)/docker-compose.sh  $ACS_MASTER0:~/polyglot/docker-compose.sh
 ssh $ACS_MASTER0 -A -p 2200 'chmod +x ~/polyglot/docker-compose.sh && ~/polyglot/docker-compose.sh'
 
-kill $SSH_AGENT_PID
+# Kill all the ssh-agent processes for now.
+ps -ef | grep ssh-agent | grep -v grep | awk '{print $2}' | xargs kill
