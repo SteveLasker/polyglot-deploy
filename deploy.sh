9722356981ffa0c1c67b0f1896b7556e9f59b017
David Zhao <dazhao@microsoft.com>
2016-06-17 14:04:25 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ scp -P 2200 $(pwd)/docker-compose.yml $ACS_MASTER0:~/polyglot/docker-compose.yml
 scp -P 2200 $(pwd)/docker-compose.sh  $ACS_MASTER0:~/polyglot/docker-compose.sh
 ssh $ACS_MASTER0 -A -p 2200 'chmod +x ~/polyglot/docker-compose.sh && ~/polyglot/docker-compose.sh'
 
-kill $SSH_AGENT_PID
+#kill $SSH_AGENT_PID
