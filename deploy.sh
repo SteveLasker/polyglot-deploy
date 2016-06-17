43afebe114206d9f461f8fec25f5f50765793b2a
David Zhao <dazhao@microsoft.com>
2016-06-17 12:45:36 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
 eval `ssh-agent`
 ssh-add $ACS_SSHKEY
 
-scp -P 2200 $BUILD_SOURCESDIRECTORY/docker-compose.yml $ACS_MASTER0:~/polyglot/docker-compose.yml
-scp -P 2200 $BUILD_SOURCESDIRECTORY/docker-compose.sh  $ACS_MASTER0:~/polyglot/docker-compose.sh
+scp -P 2200 $(pwd)/docker-compose.yml $ACS_MASTER0:~/polyglot/docker-compose.yml
+scp -P 2200 $(pwd)/docker-compose.sh  $ACS_MASTER0:~/polyglot/docker-compose.sh
 ssh $ACS_MASTER0 -A -p 2200 'chmod +x ~/polyglot/docker-compose.sh && ~/polyglot/docker-compose.sh'
